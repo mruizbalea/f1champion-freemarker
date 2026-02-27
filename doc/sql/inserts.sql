f241af5639d4259df945d7d0797ff04fbec2735f
mdasilvcos1 <mdasilvcos11@educacion.navarra.es>
2026-02-27 09:51:08 +0100

    cambios sql

--- doc/sql/inserts.sql
+++ doc/sql/inserts.sql
@@ -1,12 +1,22 @@
 
-INSERT INTO usuarios (username, password, rol, creado, modificado)
-VALUES ('Alonso', 'magic14', 0, '2024-04-29 10:00:00', '2024-04-29 10:00:00');
 
-INSERT INTO usuarios (username, password, rol, creado, modificado)
-VALUES ('Hamilton', 'sir44', 0, '2024-04-29 11:00:00', '2024-04-29 11:00:00');
+USE f1champion;
 
-INSERT INTO usuarios (username, password, rol, creado, modificado)
-VALUES ('Verstappen', 'max33', 0, '2024-04-29 12:00:00', '2024-04-29 12:00:00');
 
-INSERT INTO usuarios (username, password, rol, creado, modificado)
-VALUES ('Leclerc', 'charles16', 0, '2024-04-29 13:00:00', '2024-04-29 13:00:00');
+INSERT INTO usuarios (id, username, password, rol)
+VALUES (1, 'Alonso', 'magic14', 0);
+
+
+INSERT INTO usuarios (id, username, password, rol)
+VALUES (2, 'Hamilton', 'sir44', 0);
+
+
+INSERT INTO usuarios (id, username, password, rol)
+VALUES (3, 'Verstappen', 'max33', 0);
+
+INSERT INTO usuarios (id, username, password, rol)
+VALUES (4, 'Leclerc', 'charles16', 0);
+
+
+INSERT INTO usuarios (id, username, password, rol)
+VALUES (5, 'Adrian_Admin', 'admin123', 1);
